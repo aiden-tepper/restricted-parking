f90bf974916f0e5551264c2d36a367a46b042a78
Aiden Tepper <aidenjtep@gmail.com>
2017-06-29 17:07:38 -0700

    Added some comments

--- importer.py
+++ importer.py
@@ -10,7 +10,7 @@ from importers.foursquare import Foursquare
 
 
 class Importer():
-  
+
   # Instance Variables
   search_offset = 5000
   search_boxes = {
@@ -19,51 +19,56 @@ class Importer():
       [34.05056, -118.39777]
     ]
   }
-  
+
   venues = []
   importers = [
     Yelp(), Google(), Foursquare()
   ]
-  
+
   # Import Method
   def run(self):
     # Fetch venues for point
-    for name, box in self.search_boxes.items():     
-      for location in self.locations(box):
+    for name, box in self.search_boxes.items():
+      for location in self.locations(box): # for each grid dot
         self.fetch(location[0], location[1])
 
-  
-  def fetch(self, lat, lng):    
+
+  def fetch(self, lat, lng):
     for importer in self.importers:
       importer.fetch(lat, lng)
       importer.upload(lat, lng)
-         
-  
+
+
   def locations(self, box):
     locations = []
-    R = 6378137
-    lats = sorted([box[0][0], box[1][0]])
-    lngs = sorted([box[0][1], box[1][1]])
+    R = 6378137 # earth's radius
+
+    lats = sorted([box[0][0], box[1][0]]) # sorts x corners (latitude)
+    lngs = sorted([box[0][1], box[1][1]]) # sorts y corners (longitude)
     lat = lats[0]
     latIncr = 0
-    
-    while lat < lats[1]:  
+
+    while lat < lats[1]:
       latOffset = self.search_offset * latIncr
-      lat = lat + latOffset/R * 180/math.pi 
+      lat = lat + latOffset/R * 180/math.pi
       latIncr += 1
-      
+
       lng = lngs[0]
       lngIncr = 0
-      
-      while lng < lngs[1]:   
-        lngOffset = self.search_offset * lngIncr
-        lng = lng + lngOffset/(R * math.cos(math.pi  * lat/180)) * 180/math.pi 
-        lngIncr += 1    
-        locations.append([lat, lng])
 
-    return locations
-    
-    
+      while lng < lngs[1]:
+        lngOffset = self.search_offset * lngIncr
+        lng = lng + lngOffset/(R * math.cos(math.pi  * lat/180)) * 180/math.pi
+        lngIncr += 1
+        locations.append([lat, lng])
+        """
+        ---->
+        ---->^
+        """
+
+    return locations # returns grid of coordinate points between corners
+
+
 if __name__ == "__main__":
   if len(sys.argv) <= 1:
     print("Please provide a ParkMe API! Ex: http://api.parkme.com")
